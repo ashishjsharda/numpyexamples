ae11c07b605a5ec48546e22f0c4e950ff04d9257
Ashish S <ashishjsharda@gmail.com>
2020-11-03 06:20:42 -0500

    Create diag3.py

---
+++ diag3.py
@@ -0,0 +1,7 @@
+import numpy as np
+#Below function generates matrix with 1 on diagonal
+m=np.eye(4,k=1)
+print(m)
+#Construct a matrix with arbitary 1 d array
+diag=np.diag(np.arange(0,20,5))
+print(diag)
