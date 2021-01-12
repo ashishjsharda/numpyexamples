00d463843d318c83ee78e88c2382e09b2a2bff52
Ashish S <ashishjsharda@gmail.com>
2021-01-12 05:56:06 -0500

    Create meanvarstd.py

---
+++ meanvarstd.py
@@ -0,0 +1,7 @@
+import numpy as np
+mean = np.mean([10,20,30,40])
+print("Mean seen is ",mean)
+var=np.var([10,20,30,40])
+print("Variance seen is ",var)
+std=np.std([10,20,30,40])
+print("Std Deviation seen is ",std)
